4113d25d7747ed29e455853c8177b89f1fcef4b5
Tran Doan Anh Quan (FUSA.PRES) <QuanTDA1@fsoft.com.vn>
2018-05-28 09:12:04 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Overview
+A simple Java FX 8 application, with:
+1. One main stage and 2 dialog stages
+2. Using Anchor Pane, Border Pane
+3. Using TableView, Label, TextFields
+4. JAXB to save data into an XML file, bundled with LocalDateAdapter to format LocalDate into XML strings.
+5. Built DateUtil to convert LocalDate into String et vice versa.
+6. Using SceneBuilder 8.0103 bundled by Glueon to build screens
+
+# References
+Following the tutorial link: [JavaFX 8 Tutorial] (http://code.makery.ch/library/javafx-8-tutorial/) 
---
+++ src/main/java/vn/quantda/javafx/addressapp/viewcontroller/BirthdayStatisticsController.java
@@ -0,0 +1,63 @@
+package vn.quantda.javafx.addressapp.viewcontroller;
+
+import java.text.DateFormatSymbols;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.XYChart;
+import vn.quantda.javafx.addressapp.model.Person;
+
+public class BirthdayStatisticsController {
+
+	@FXML
+	private BarChart<String, Integer> barChart;
+	
+	@FXML
+	private CategoryAxis xAxis;
+	
+	private ObservableList<String> monthNames = FXCollections.<String>observableArrayList();
+	
+	/**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+	@FXML
+	private void initialize() {
+		// Get an array with the English month names.
+		String[] months = DateFormatSymbols.getInstance(Locale.ENGLISH).getMonths();
+		// Convert it to a list and add it to our ObservableList of months.
+		monthNames.addAll(Arrays.asList(months));
+		
+		// Assign the month names as categories for the horizontal axis.
+		xAxis.setCategories(monthNames);
+	}
+	
+	/**
+     * Sets the persons to show the statistics for.
+     * 
+     * @param persons
+     */
+	public void setPersonData(List<Person> persons) {
+		// Count the number of people having their birthday in a specific month.
+		int[] monthCounter = new int[12];
+		for (Person person : persons) {
+			int month = person.getBirthday().getMonthValue() - 1;
+			monthCounter[month]++;
+		}
+		
+		XYChart.Series<String, Integer> series = new XYChart.Series<>();
+		
+		// Create a XYChart.Data object for each month. Add it to the series.
+		for (int i = 0; i < monthCounter.length; i++) {
+            series.getData().add(new XYChart.Data<>(monthNames.get(i), monthCounter[i]));
+        }
+
+        barChart.getData().add(series);
+	}
+}
